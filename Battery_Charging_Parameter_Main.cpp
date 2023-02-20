b63cacac25c5da799c3a51bef55848a894d69733
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2023-02-20 10:42:26 +0530

    Update Battery_Charging_Parameter_Main.cpp

--- Battery_Charging_Parameter_Main.cpp
+++ Battery_Charging_Parameter_Main.cpp
@@ -1,12 +1,15 @@
 #include "Battery_Charging_Parameter.hpp"
 
-int main() {
-    float min_Soc = 20.0;
-    float max_Soc = 80.0;
-    float min_Temperature = 0.0;
-    float min_Temperature = 45.0;
-    std::cout<<batteryParameterReadings(min_Soc, max_Soc);
-  std::cout<<batteryParameterReadings(min_Temperature, min_Temperature);
-  
-    return 0;
+int main()
+{
+  float min_Soc = 20.0;
+  float max_Soc = 80.0;
+  float min_Temperature = 0.0;
+  float max_Temperature = 45.0;
+  std::cout << "Battery_Charging_Parameter \n"
+            << "SOC: " << batteryParameterReadings(min_Soc, max_Soc) << "\n";
+  std::cout << "Battery_Charging_Parameter \n"
+            << "Temperature: " <<batteryParameterReadings(min_Temperature, max_Temperature) << "\n";
+
+  return 0;
 }
